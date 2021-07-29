200df51f44119178345dafee776d9e6b1eba56f5
Mike DelGaudio <mdelgaud@stevens.edu>
2021-07-28 21:01:02 -0400

    adding modify entry

--- cloudflare.js
+++ cloudflare.js
@@ -10,6 +10,14 @@ const CLOUDFLARE_HEADERS = {
   "Content-Type": "application/json",
 };
 
+const log = (message) => {
+  console.log(
+    `${new Date().toLocaleString("en-US", {
+      timeZone: "America/New_York",
+    })} - ${message}`
+  );
+};
+
 const handleError = (err) => {
   console.warn(err);
   return new Response(
@@ -20,6 +28,7 @@ const handleError = (err) => {
 };
 
 const getDnsState = async (requested_url) => {
+  if (!requested_url) return;
   const url = `${CLOUDFLARE_PREFIX}zones/${process.env?.CLOUDFLARE_ZONE_ID}/dns_records?name=${requested_url}`;
   const response = await fetch(url, {
     method: "GET",
@@ -31,6 +40,7 @@ const getDnsState = async (requested_url) => {
 };
 
 const modifyDnsIp = async (record) => {
+  //if (!record.id || !record) return;
   const url = `${CLOUDFLARE_PREFIX}zones/${process.env?.CLOUDFLARE_ZONE_ID}/dns_records/${record?.id}`;
   const response = await fetch(url, {
     method: "PUT",
@@ -43,8 +53,9 @@ const modifyDnsIp = async (record) => {
   return await response.json();
 };
 
-const getDnsHealth = async (url) => {
-  const response = await fetch(url, {
+const getDnsHealth = async (requested_url) => {
+  if (!requested_url) return;
+  const response = await fetch(requested_url, {
     method: "GET",
   }).catch((err) => {
     console.log(err);
@@ -52,22 +63,92 @@ const getDnsHealth = async (url) => {
     //handleError(err);
     return;
   });
-  return await response;
+
+  if (response.ok) {
+    return await response.json();
+  } else {
+    return Promise.reject(
+      `Error ${response.status}, unable to resolve website.`
+    );
+  }
 };
 
 // const l = getDnsState("apibullishbay.delgaudiomike.com");
 // console.log(l.then((d) => console.log(d)));
 
-// const test = modifyDnsIp({
-//   id: "",
-//   type: "A",
-//   name: "we",
-//   content: process.env?.SECONDARY_IP,
-//   ttl: 1,
-//   proxied: true,
-// });
-
 // test.then((d) => console.log(d));
 
-const tester = getDnsHealth("https://bullishbay.delgaudiomike.com");
-tester.then((d) => console.log(d)).catch((e) => e);
+// const tester = getDnsHealth("https://bulli2shbay.delgaudiomike.com");
+// tester.then((d) => console.log(d)).catch((e) => e);
+
+/**
+ * Switches the provided site name to the primary DNS IP
+ * @param {string} id cloudflare provided DNS ID
+ * @param {string} site website to switch DNS IP for
+ */
+const switchToPrimary = async (id, site) => {
+  await modifyDnsIp({
+    id,
+    type: "A",
+    name: site,
+    content: process?.env?.PRIMARY_IP,
+    ttl: 1,
+    proxied: true,
+  }).catch(() => {
+    handleModifyError(site, "PRIMARY");
+    return;
+  });
+  handleModifySuccess(site, "PRIMARY");
+};
+
+/**
+ * Switches the provided site name to the secondary DNS IP
+ * @param {string} id cloudflare provided DNS ID
+ * @param {string} site website to switch DNS IP for
+ */
+const switchToSecondary = async (id, site) => {
+  await modifyDnsIp({
+    id,
+    type: "A",
+    name: site,
+    content: process?.env?.SECONDARY_IP,
+    ttl: 1,
+    proxied: true,
+  }).catch(() => {
+    handleModifyError(site, "SECONDARY");
+    return;
+  });
+  handleModifySuccess(site, "SECONDARY");
+};
+
+const handleModifySuccess = (site, location) => {
+  log(`Successfully swapped DNS entry to ${location} for: ${site}`);
+};
+
+const handleModifyError = (site, location) => {
+  log(`Unable to modify DNS entry to ${location} for: ${site}`);
+};
+
+const SUBDOMAINS = {
+  neu: process?.env?.CF_NEU_ID,
+  weatherflash: process?.env?.CF_WEATHERFLASH_ID,
+  bullishbay: process?.env?.CF_BULLISHBAY_ID,
+  apibullishbay: process?.env?.CF_APIBULLISHBAY_ID,
+};
+
+const DOMAINS = {
+  "delgaudiomike.com": process?.env?.CF_DELGAUDIOMIKE_ID,
+};
+
+const healthcheck = async () => {
+  for (const site in SUBDOMAINS) {
+    console.log(`Testing now: https://${site}.delgaudiomike.com`);
+  }
+  for (const site in DOMAINS) {
+    console.log(`Testing now: https://${site}`);
+  }
+};
+
+switchToSecondary("", "test");
+
+// setInterval(healthcheck, 300000);
